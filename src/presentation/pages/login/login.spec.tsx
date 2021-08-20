61145e24bedb623a44267bd2f9c1fd3b5167e5e2
Pedro Vaz <ppvaz03@gmail.com>
2021-08-20 16:58:39 -0300

    test: ensure Login starts with inputs required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -9,5 +9,11 @@ describe('Login Component', () => {
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.className).toContain('inputRedStatus')
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.className).toContain('inputRedStatus')
   })
 })
