09d33b8e84c5bd3eac0a1c7b2dda43ec32eea4ac
Pedro Vaz <ppvaz03@gmail.com>
2021-09-05 16:53:55 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -2,16 +2,30 @@ import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidation } from '@/validation/validators/email/email-validation'
 import faker from 'faker'
 
+type SutTypes = {
+  sut: EmailValidation
+  fieldName: string
+}
+
+const makeSut = (): SutTypes => {
+  const fieldName = faker.database.column()
+  const sut = new EmailValidation(fieldName)
+
+  return {
+    sut,
+    fieldName
+  }
+}
+
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const fieldName = faker.random.word()
-    const sut = new EmailValidation(fieldName)
+    const { sut, fieldName } = makeSut()
     const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError(fieldName))
   })
 
   test('Should return false if email is valid', () => {
-    const sut = new EmailValidation(faker.random.word())
+    const { sut } = makeSut()
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
