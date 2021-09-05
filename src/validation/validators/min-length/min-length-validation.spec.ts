7affcd3d06b37974b60f4eb74721ceeb0ee8a3cb
Pedro Vaz <ppvaz03@gmail.com>
2021-09-05 17:02:46 -0300

    test: ensure MinLengthValidation returns error if value doesn't reach minimum length

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,12 @@
+import { InvalidFieldError } from '@/validation/errors'
+import faker from 'faker'
+import { MinLengthValidation } from './min-length-validation'
+
+describe('MinLengthValidation', () => {
+  test('Should return error if value doesn\'t reach minimum length', () => {
+    const fieldName = faker.database.column()
+    const sut = new MinLengthValidation(fieldName, 5)
+    const error = sut.validate('123')
+    expect(error).toEqual(new InvalidFieldError(fieldName))
+  })
+})
