17326f93a6ffd32819bb88cbfc2e3b7e569def96
Pedro Vaz <ppvaz03@gmail.com>
2021-09-08 22:06:04 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -21,4 +21,15 @@ describe('ValidationBuilder', () => {
     const validations = sut.field(fieldName).min(length).build()
     expect(validations).toEqual([new MinLengthValidation(fieldName, length)])
   })
+
+  test('Should return a list of validations', () => {
+    const fieldName = faker.database.column()
+    const length = faker.random.number()
+    const validations = sut.field(fieldName).required().min(length).email().build()
+    expect(validations).toEqual([
+      new RequiredFieldValidation(fieldName),
+      new MinLengthValidation(fieldName, length),
+      new EmailValidation(fieldName)
+    ])
+  })
 })
