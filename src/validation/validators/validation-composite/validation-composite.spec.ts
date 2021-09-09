46460cbe5c7049a06d63488176d11ecb6e2cf62e
Pedro Vaz <ppvaz03@gmail.com>
2021-09-08 22:13:46 -0300

    refactor: change ValidationComposite to use a static build method to create instance

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -16,7 +16,7 @@ const makeSut = (): SutTypes => {
     new FieldValidationSpy(fieldName)
   ]
 
-  const sut = new ValidationComposite(fieldValidationSpies)
+  const sut = ValidationComposite.build(fieldValidationSpies)
 
   return {
     sut,
